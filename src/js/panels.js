da1e4d313d69296a3ae4af6d8be56f91df36729f
ruckel <kevin.dybeck@gmail.com>
2023-03-31 00:47:53 +0200

    testing url stuff

--- src/js/panels.js
+++ src/js/panels.js
@@ -6,24 +6,24 @@ function panel() {
     const header = document.querySelector("#panel");
     const path = window.location.pathname;
   
-    if (path === "../src/") {
-      window.location.pathname = "../src/index";
+    if (path === "../") {
+      window.location.pathname = "../index";
     }
   
     header.innerHTML = `
       <nav class="nav">
         <ul>
           <li class="${path.includes("/contact.html") ? "active" : ""}">
-            <a href="../src/contact">Contact us</a>
+            <a href="../contact">Contact us</a>
           </li>
           <li class="${path.includes("/produkter.html") ? "active" : ""}">
-            <a href="../src/produkter">Products</a>
+            <a href="../produkter">Products</a>
           </li>
           <li class="${path.includes("/order.html") ? "active" : ""}">
-            <a href="../src/order">Order</a>
+            <a href="../order">Order</a>
           </li>
           <li class="${path.includes("/index.html") ? "active" : ""}">
-            <a href="../src/">Home</a>
+            <a href="../">Home</a>
           </li>
         </ul>
       </nav>
